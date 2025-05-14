1f90982ba992e72f16bea8a3db02f29c99fed559
OswinCuaran <ocuaranc@unal.edu.co>
2025-05-13 19:33:35 -0500

    Trabajo Terminado

--- java.js
+++ java.js
@@ -1,13 +1,26 @@
-let productoSeleccionado = "";
+let productoSeleccionado = " ";
+let imagenHTML = " ";
 
 function mostrarProducto(){
     var select = document.getElementById("producto")
     productoSeleccionado = select.value;
 
     var card = document.getElementById("card1");
+
+    
+    if (productoSeleccionado === "Camisa") {
+        imagenHTML = `<img src="img/camisa.jpg" alt="">`;
+    } else if (productoSeleccionado === "Pantalon"){
+        imagenHTML = `<img src="img/pantalon.jpg" alt="">`;	
+    } else if (productoSeleccionado === "Zapatos"){
+        imagenHTML = `<img src="img/zapato.jpg" alt="">`;	
+    } else if (productoSeleccionado === "Chaqueta"){
+        imagenHTML = `<img src="img/chaqueta.jpg" alt="">`;
+    }
     card.innerHTML = 
         `<div>
             <h2>Categoria de Productos</h2>
+            ${imagenHTML}
             <h2 id="nombre">${productoSeleccionado}</h2>
             <label for="input1">Seleccione la Cantidad </label>
             <input type="number" id="input1">
